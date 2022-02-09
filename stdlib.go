0131f799f7f140dad8ed318303e56d2431eb7d63
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-02-09 03:54:45 -0500

    chg: PathExist returns false for all errors in path.

--- stdlib.go
+++ stdlib.go
@@ -80,9 +80,9 @@ func (fec *FileExtChecker) IsValid(file string) (ret bool) {
 func PathExist(filename string) bool {
 	_, err := os.Stat(filename)
 
-	if os.IsNotExist(err) {
-		return false
+	if err == nil {
+		return true
 	}
 
-	return true
+	return false
 }
