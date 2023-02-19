2106eab1a354fd428147db00651ca2892ce45411
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2023-02-18 23:35:54 -0500

    fix: File Extension Checker File Handling
    Improve handling of passing the file checker full paths, hidden
    directories, and default to include all files so only an exclusion list
    is needed. Also if a file has no extension, then use its basename.

--- stdlib_test.go
+++ stdlib_test.go
@@ -28,22 +28,23 @@ func TestPathExist(runner *testing.T) {
 }
 
 func TestIsTextFile(tester *testing.T) {
-
 	cases := []struct {
 		name, path string
 		want       bool
 	}{
-		{"IsATextFile", "text-file-01.txt", true},
-		{"notATextFile", "text-file-02.jpg", false},
-		{"notATextFile", "text-file-03.gif", false},
-		{"notATextFile", "text-file-04.png", false},
-		{"notATextFile", "text-file-05.json", true},
-		{"notATextFile", "text-file-06.md", true},
-		{"notATextFile", "text-file-07.xml", true},
+		{"txt", "text-file-01.txt", true},
+		{"jpg", "text-file-02.jpg", false},
+		{"gif", "text-file-03.gif", false},
+		{"png", "text-file-04.png", false},
+		{"json", "text-file-05.json", true},
+		{"md", "text-file-06.md", true},
+		{"xml", "text-file-07.xml", true},
+		{"hiddenDirectory", ".hidden/file.txt", true},
+		{"noExtension", "config", true},
 	}
 
 	el := []string{"jpg", "gif", "png", "pdf"}
-	in := []string{"txt", "json", "md", "xml"}
+	in := []string{"txt", "json", "md", "xml", "config"}
 	sbj, _ := NewFileExtChecker(&el, &in)
 
 	for _, fxtr := range cases {
@@ -66,25 +67,6 @@ func TestIsTextFile(tester *testing.T) {
 	fxtr := cases[0]
 	el = []string{"jpg", "gif", "png", "pdf"}
 	in = []string{}
-	sbj, _ = NewFileExtChecker(&el, &in)
-	tester.Run(fxtr.name, func(t *testing.T) {
-
-		got := sbj.IsValid(fxtr.path)
-
-		if got != fxtr.want {
-			t.Errorf("got %v, want %v, for %v", got, fxtr.want, fxtr.path)
-		}
-	})
-
-	cases = []struct {
-		name, path string
-		want       bool
-	}{
-		{"notInTheIncludeList", "file-07.jpg", false},
-	}
-	fxtr = cases[0]
-	el = []string{}
-	in = []string{"txt", "json", "md", "xml"}
 	sbj, _ = NewFileExtChecker(&el, &in)
 	tester.Run(fxtr.name, func(t *testing.T) {
 
