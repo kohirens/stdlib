1347b3f57f3a2e2457606e55b86b8f6d467c5c5b
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-05-28 05:27:33 -0400

    add: Method To Setup A Tmp Git Repository
    During a unit test setup a git repository from a bundle to use as a
    fixture during a test case.

---
+++ test/git.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+	"fmt"
+	"github.com/kohirens/stdlib"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func SetupARepository(bundleName, tmpDir, bundleDir, ps string) string {
+	repoPath := tmpDir + ps + bundleName
+
+	// It may have already been unbundled.
+	fileInfo, err1 := os.Stat(repoPath)
+	if (err1 == nil && fileInfo.IsDir()) || os.IsExist(err1) {
+		absPath, e2 := filepath.Abs(repoPath)
+		if e2 == nil {
+			return absPath
+		}
+		return repoPath
+	}
+
+	wd, e := os.Getwd()
+	if e != nil {
+		panic(fmt.Sprintf("%v failed to get working directory", e.Error()))
+	}
+
+	srcRepo := wd + ps + bundleDir + ps + bundleName + ".bundle"
+	// It may not exist.
+	if !stdlib.PathExist(srcRepo) {
+		panic(fmt.Sprintf("%v bundle not found", srcRepo))
+	}
+
+	cmd := exec.Command("git", "clone", "-b", "main", srcRepo, repoPath)
+	_, _ = cmd.CombinedOutput()
+	if ec := cmd.ProcessState.ExitCode(); ec != 0 {
+		log.Panicf("error un-bundling %q to %q for a unit test", srcRepo, repoPath)
+	}
+
+	absPath, e2 := filepath.Abs(repoPath)
+	if e2 != nil {
+		panic(e2.Error())
+	}
+
+	return absPath
+}
