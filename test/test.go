ff3124264b036ee13d864e8d48375679f8c90d97
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-05-28 05:43:14 -0400

    add: More Testing Tools
    These tools have already proven useful in several separate projects and
    this will allow them to be served and maintained from a central location.

---
+++ test/test.go
@@ -0,0 +1,66 @@
+package test
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+// GetTestBinCmd return a command to run the test binary in a sub-process, passing it flags as fixtures to produce expected output; `TestMain`, will be run automatically.
+func GetTestBinCmd(subEnvVarName string, args []string) *exec.Cmd {
+	// call the generated test binary directly
+	// Have it the function runAppMain.
+	cmd := exec.Command(os.Args[0])
+	// Run in the context of the source directory.
+	_, filename, _, _ := runtime.Caller(0)
+	cmd.Dir = path.Dir(filename)
+	// Set an environment variable
+	// 1. Only exist for the life of the test that calls this function.
+	// 2. Passes arguments/flag to your app
+	// 3. Lets TestMain know when to run the main function.
+	subCmdFlags := subEnvVarName + "=" + strings.Join(args, " ")
+	cmd.Env = append(os.Environ(), subCmdFlags)
+
+	return cmd
+}
+
+// Silencer return a function that prevents output during a test run.
+func Silencer() func() {
+	// Abort in verbose mode.
+	if testing.Verbose() {
+		return func() {}
+	}
+	null, _ := os.Open(os.DevNull)
+	sOut := os.Stdout
+	sErr := os.Stderr
+	os.Stdout = null
+	os.Stderr = null
+	log.SetOutput(null)
+	return func() {
+		defer null.Close()
+		os.Stdout = sOut
+		os.Stderr = sErr
+		log.SetOutput(os.Stderr)
+	}
+}
+
+func VerboseSubCmdOut(stdo []byte, stde error) ([]byte, error) {
+	// Debug
+	if testing.Verbose() {
+		fmt.Print("\nBEGIN sub-command\n")
+		fmt.Printf("stdout:\n%s\n", stdo)
+
+		if stde != nil {
+			fmt.Printf("stderr:\n%v\n", stde.Error())
+		}
+
+		fmt.Print("\nEND sub-command\n\n")
+	}
+
+	return stdo, stde
+}
