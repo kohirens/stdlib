00c0701239b6211ca9d6fe36c7adefd45f22371a
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-06-08 11:26:30 -0400

    add: Run A Command With Input
    Allows piping input into a command in a sub process.

--- cli/cli.go
+++ cli/cli.go
@@ -19,3 +19,34 @@ func RunCommand(program, workDir string, args ...string) (cmdOut []byte, cmdErr 
 	log.Infof("sub command run: %q", cmd.String())
 	return
 }
+
+// RunCommandWithInput run an external program.
+func RunCommandWithInput(workDir string, input []byte, program string, args ...string) (cmdOut []byte, cmdErr error, exitCode int) {
+	cmd := exec.Command(program, args...)
+	cmd.Env = os.Environ()
+	cmd.Dir = workDir
+
+	if input != nil {
+		cmdIn, err1 := cmd.StdinPipe()
+		if err1 != nil {
+			return []byte{}, err1, 1
+		}
+
+		defer func() {
+			_ = cmdIn.Close()
+		}()
+
+		// write the input
+		_, err2 := cmdIn.Write(input)
+		if err2 != nil {
+			return []byte{}, err1, 1
+		}
+	}
+
+	cmdOut, cmdErr = cmd.CombinedOutput()
+	exitCode = cmd.ProcessState.ExitCode()
+
+	log.Infof("sub command run: %q", cmd.String())
+
+	return
+}
