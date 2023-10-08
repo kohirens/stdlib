8d7302358942ca180257f32d7202361928a97e6b
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-10-08 15:38:11 -0400

    add: More Git Functions
    Migrated Git functionality from tmpltoapp.
    * Clone - clone a repository.
    * Checkout - checkout a git ref.
    * GetLastCommitHash - Return the HEAD commit hash.
    * GetLatestTag - Return the latest tag after sorting by sematic
      versioning.
    * GetRemoteTags - Get a list of remote tags.

--- git/git.go
+++ git/git.go
@@ -1,13 +1,100 @@
 package git
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/kohirens/stdlib/log"
 	"github.com/kohirens/stdlib/path"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strings"
 )
+
+// Checkout Open an existing repo and checkout commit by full ref-name
+func Checkout(repoLocalPath, ref string) (string, string, error) {
+	log.Logf("pulling latest\n")
+	_, e1 := gitCmd(repoLocalPath, "fetch", "--all", "-p")
+	if e1 != nil {
+		return "", "", fmt.Errorf(stderr.GitFetchFailed, repoLocalPath, ref, e1.Error())
+	}
+
+	log.Infof(stdout.RefInfo, ref)
+	log.Infof(stdout.GitCheckout, ref)
+
+	_, e2 := gitCmd(repoLocalPath, "checkout", ""+ref)
+	if e2 != nil {
+		return "", "", fmt.Errorf(stderr.GitCheckoutFailed, e2.Error())
+	}
+
+	repoDir, e8 := filepath.Abs(repoLocalPath)
+	if e8 != nil {
+		return "", "", e8
+	}
+
+	latestCommitHash, e4 := HeadCommitHash(repoDir)
+	if e4 != nil {
+		return "", "", e4
+	}
+
+	return repoDir, latestCommitHash, nil
+}
+
+// Clone Use git application to clone a repo from one location to a local
+// directory.
+func Clone(repoUri, repoDir, refName string) (string, string, error) {
+	log.Infof("branch to clone is %q", refName)
+	log.Infof("git clone %s", repoUri)
+
+	var sco []byte
+	var e1 error
+
+	if isRemoteRepo(repoUri) {
+		branchName := refName
+		re := regexp.MustCompile("^refs/[^/]+/(.*)$")
+		if re.MatchString(refName) {
+			branchName = re.ReplaceAllString(refName, "${1}")
+		}
+
+		log.Logf("cloning branch name: %v", branchName)
+
+		// git clone --depth 1 --branch <tag_name> <repo_url>
+		// NOTE: Branch cannot be a full ref but can be short ref name or a tag.
+		sco, e1 = gitCmd(".", "clone", "--depth", "1", "--branch", branchName, repoUri, repoDir)
+	} else {
+		// git clone <repo_url>
+		sco, e1 = gitCmd(".", "clone", repoUri, repoDir)
+		if e1 != nil {
+			return "", "", fmt.Errorf(stderr.Cloning, repoUri, e1.Error())
+		}
+
+		log.Infof("clone output \n%s", sco)
+
+		// get current branch
+		cb, e4 := gitCmd(repoDir, "branch", "--show-current", refName)
+		if e4 != nil {
+			return "", "", fmt.Errorf(stderr.CurrentBranch, repoUri, e4.Error(), cb)
+		}
+		// skip if already on desired branch
+		if strings.Trim(bytes.NewBuffer(cb).String(), "\r\n") != refName {
+			// git checkout <ref_name>
+			co, e3 := gitCmd(repoDir, "checkout", "-b", refName)
+			if e3 != nil {
+				return "", "", fmt.Errorf(stderr.Checkout, repoUri, e3.Error(), co)
+			}
+
+			log.Infof("checkout output \n%s", co)
+		}
+	}
+
+	latestCommitHash, e2 := HeadCommitHash(repoDir)
+	if e2 != nil {
+		return "", "", fmt.Errorf(stderr.GettingCommitHash, repoDir, e2.Error())
+	}
+
+	return repoDir, latestCommitHash, nil
+}
 
 // CloneFromBundle Set up a repository from a git bundle.
 func CloneFromBundle(bundleName, tmpDir, bundleDir, ps string) string {
@@ -37,7 +124,7 @@ func CloneFromBundle(bundleName, tmpDir, bundleDir, ps string) string {
 	cmd := exec.Command("git", "clone", "-b", "main", srcRepo, repoPath)
 	_, _ = cmd.CombinedOutput()
 	if ec := cmd.ProcessState.ExitCode(); ec != 0 {
-		log.Panicf("error un-bundling %q to %q for a unit test", srcRepo, repoPath)
+		log.Panf("error un-bundling %q to %q for a unit test", srcRepo, repoPath)
 	}
 
 	absPath, e2 := filepath.Abs(repoPath)
@@ -46,4 +133,85 @@ func CloneFromBundle(bundleName, tmpDir, bundleDir, ps string) string {
 	}
 
 	return absPath
+}
+
+// HeadCommitHash Returns the HEAD commit hash.
+func HeadCommitHash(repoDir string) (string, error) {
+	latestCommitHash, e1 := gitCmd(repoDir, "rev-parse", "HEAD")
+	if e1 != nil {
+		return "", fmt.Errorf(stderr.GettingCommitHash, repoDir, e1.Error())
+	}
+
+	return strings.Trim(string(latestCommitHash), "\n"), nil
+}
+
+// LatestTag Will return the latest tag or an empty string from a repository.
+func LatestTag(repoDir string) (string, error) {
+	tags, e1 := RemoteTags(repoDir)
+	if e1 != nil {
+		return "", fmt.Errorf(stderr.GetLatestTag, repoDir, e1.Error())
+	}
+
+	return tags[0], nil
+}
+
+// RemoteTags Get the remote tags on a repo using git ls-remote.
+func RemoteTags(repo string) ([]string, error) {
+	// Even without cloning or fetching, you can check the list of tags on the upstream repo with git ls-remote:
+	sco, e1 := gitCmd(repo, "ls-remote", "--sort=-version:refname", "--tags")
+	if e1 != nil {
+		return nil, fmt.Errorf(stderr.GetRemoteTags, e1.Error())
+	}
+
+	reTags := regexp.MustCompile("[a-f0-9]+\\s+refs/tags/(\\S+)")
+	mat := reTags.FindAllSubmatch(sco, -1)
+	if mat == nil {
+		return nil, fmt.Errorf("%s", "no tags found")
+	}
+
+	ret := make([]string, len(mat))
+	for i, v := range mat {
+		log.Dbugf(stdout.RemoteTagDbug1, string(v[1]))
+		ret[i] = string(v[1])
+	}
+
+	return ret, nil
+}
+
+// gitCmd run a git command.
+func gitCmd(repoPath string, args ...string) ([]byte, error) {
+	cmd := exec.Command("git", args...)
+	cmd.Env = os.Environ()
+	cmd.Dir = repoPath
+	cmdStr := cmd.String()
+
+	log.Infof(stdout.RunningCommand, cmdStr)
+
+	cmdOut, cmdErr := cmd.CombinedOutput()
+	exitCode := cmd.ProcessState.ExitCode()
+
+	if cmdErr != nil {
+		return nil, fmt.Errorf(stderr.RunGitFailed, args, cmdErr.Error(), cmdOut)
+	}
+
+	if exitCode != 0 {
+		return nil, fmt.Errorf(stderr.GitExitErrCode, args, exitCode)
+	}
+
+	return cmdOut, nil
+}
+
+// isRemoteRepo return true if Git repository is a remote URL or false if local.
+func isRemoteRepo(repoLocation string) bool {
+	if len(repoLocation) < 1 {
+		return false
+	}
+	// git@github.com:kohirens/tmpltoap.git
+	// https://github.com/kohirens/tmpltoapp.git
+	isGitUri := regexp.MustCompile("^(git|http|https)://.+$")
+	if isGitUri.MatchString(repoLocation) {
+		return true
+	}
+
+	return false
 }
--- git/git_test.go
+++ git/git_test.go
@@ -1,13 +1,25 @@
 package git
 
 import (
-	"github.com/kohirens/stdlib/internal/test"
+	"fmt"
 	"github.com/kohirens/stdlib/path"
+	"github.com/kohirens/stdlib/test"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
-const ps = string(os.PathSeparator)
+const (
+	FixtureDir = "testdata"
+	ps         = string(os.PathSeparator)
+	TmpDir     = "tmp"
+)
+
+func TestMain(t *testing.M) {
+	test.ResetDir(TmpDir, 0774)
+	os.Exit(t.Run())
+}
 
 func TestCloneFromBundle(t *testing.T) {
 	type args struct {
@@ -23,16 +35,214 @@ func TestCloneFromBundle(t *testing.T) {
 	}{
 		{
 			"bundle-eixists",
-			args{"repo-01", test.TestTmp, test.FixtureDir, ps},
+			args{"repo-01", TmpDir, FixtureDir, ps},
 			true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := CloneFromBundle(tt.args.bundleName, test.TestTmp, tt.args.bundleDir, tt.args.ps)
+			got := CloneFromBundle(tt.args.bundleName, TmpDir, tt.args.bundleDir, tt.args.ps)
 			if path.Exist(got) != tt.want {
 				t.Errorf("CloneFromBundle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// Clone a repo
+func TestGitClone(tester *testing.T) {
+	var testCases = []struct {
+		name      string
+		repo      string
+		outPath   string
+		branch    string
+		shouldErr bool
+		wantHash  string
+	}{
+		{
+			"cloneRepo1",
+			"repo-01.git",
+			TmpDir + ps + "repo-01-refs-heads-main",
+			"refs/heads/main",
+			false,
+			"b7e42844c597d2beaf774eddfdcb653a2a4b0050",
+		},
+	}
+
+	for _, tc := range testCases {
+		tester.Run(tc.name, func(t *testing.T) {
+			repoPath := CloneFromBundle(tc.repo, TmpDir, FixtureDir, ps)
+
+			gotPath, gotHash, err := Clone(repoPath, tc.outPath, tc.branch)
+
+			if tc.shouldErr == true && err == nil {
+				t.Error("did not get expected err")
+			}
+
+			if tc.shouldErr == false && err != nil {
+				t.Errorf("got an unexpected err: %s", err)
+			}
+
+			if gotHash != tc.wantHash {
+				t.Errorf("got %v, want %v", gotHash, tc.wantHash)
+			}
+
+			if gotPath != tc.outPath {
+				t.Errorf("got %v, want %v", gotPath, tc.outPath)
+			}
+		})
+	}
+}
+
+// Clone a repo
+func TestGitCannotClone(tester *testing.T) {
+	var testCases = []struct {
+		name      string
+		repo      string
+		outPath   string
+		branch    string
+		shouldErr bool
+		wantHash  string
+	}{
+		{"clone404", "does-not-exist.git", "", "", true, ""},
+	}
+
+	for _, tc := range testCases {
+		tester.Run(tc.name, func(t *testing.T) {
+			repoPath, _ := filepath.Abs(TmpDir)
+			repoPath += ps + tc.repo
+
+			gotPath, gotHash, err := Clone(repoPath, tc.outPath, tc.branch)
+
+			if tc.shouldErr == true && err == nil {
+				t.Error("did not get expected err")
+			}
+
+			if tc.shouldErr == false && err != nil {
+				t.Errorf("got an unexpected err: %s", err)
+			}
+
+			if gotHash != tc.wantHash {
+				t.Errorf("got %v, want %v", gotHash, tc.wantHash)
+			}
+
+			if gotPath != tc.outPath {
+				t.Errorf("got %v, want %v", gotPath, tc.outPath)
+			}
+		})
+	}
+}
+
+func TestGetRepoDir2(tester *testing.T) {
+	absTestTmp, _ := filepath.Abs(TmpDir)
+	var testCases = []struct {
+		name     string
+		bundle   string
+		branch   string
+		want     string
+		wantHash string
+	}{
+		{"localFullBranchRefArg", "repo-02", "refs/remotes/origin/third-commit", absTestTmp + ps + "repo-02", "bfeb0a45c027420e4df286dc089965599e350bf9"},
+	}
+
+	for _, tc := range testCases {
+		repoPath := CloneFromBundle(tc.bundle, TmpDir, FixtureDir, ps)
+
+		tester.Run(tc.name, func(t *testing.T) {
+			gotRepo, gotHash, gotErr := Checkout(repoPath, tc.branch)
+
+			if gotErr != nil {
+				t.Errorf("unexpected error in test %q", gotErr.Error())
+			}
+
+			if gotRepo != tc.want {
+				t.Errorf("got %v, want %v", gotRepo, tc.want)
+			}
+
+			if gotHash != tc.wantHash {
+				t.Errorf("got %v, want %v", gotHash, tc.wantHash)
+			}
+		})
+	}
+}
+
+func TestGetLatestTag(tester *testing.T) {
+	var testCases = []struct {
+		name   string
+		bundle string
+		want   string
+	}{
+		{"found", "repo-03", "0.1.0"},
+	}
+
+	for i, tc := range testCases {
+		repoPath := CloneFromBundle(tc.bundle, TmpDir, FixtureDir, ps)
+
+		tester.Run(fmt.Sprintf("%v.%v", i+1, tc.name), func(t *testing.T) {
+			got, gotErr := LatestTag(repoPath)
+
+			if gotErr != nil {
+				t.Errorf("unexpected error in test %q", gotErr.Error())
+			}
+
+			if got != tc.want {
+				t.Errorf("got %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestGetLatestTagError(tester *testing.T) {
+	var testCases = []struct {
+		name   string
+		bundle string
+		want   string
+	}{
+		{"doesNotExist", "repo-dne", ""},
+	}
+
+	for i, tc := range testCases {
+		repoPath, _ := filepath.Abs("tmp")
+		repoPath += ps + tc.bundle
+		tester.Run(fmt.Sprintf("%v.%v", i+1, tc.name), func(t *testing.T) {
+			got, gotErr := LatestTag(repoPath)
+
+			if gotErr == nil {
+				t.Errorf("unexpected error in test %q", gotErr.Error())
+			}
+
+			if got != tc.want {
+				t.Errorf("got %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestGetRemoteTags(tester *testing.T) {
+	var testCases = []struct {
+		name      string
+		bundle    string
+		want      string
+		shouldErr bool
+	}{
+		{"hasTags", "repo-04", "1.0.0,0.2.0,0.1.1,0.1.0", false},
+		{"noTags", "repo-05", "", true},
+	}
+
+	for i, tc := range testCases {
+		repoPath := CloneFromBundle(tc.bundle, TmpDir, FixtureDir, ps)
+
+		tester.Run(fmt.Sprintf("%v.%v", i+1, tc.name), func(t *testing.T) {
+			got, gotErr := RemoteTags(repoPath)
+
+			if !tc.shouldErr && gotErr != nil {
+				t.Errorf("unexpected error in test %q", gotErr.Error())
+			}
+
+			t1 := strings.Join(got, ",")
+			if got != nil && t1 != tc.want {
+				t.Errorf("got %v, want %v", t1, tc.want)
 			}
 		})
 	}
