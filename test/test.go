ab8a6d7f54fb59f965bbd09ab128a8facc8e3c05
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-10-09 07:49:52 -0400

    add: Test Features
    Upgraded the test package with more functionality.
    
    * Added a few constants for the tmp and test data directories.
    * Added RunMain for running recursive unit test to aid in testing the main app.
    * Changed GetTestBinCmd so that when the unit test -v flag is set, the sub
      process will display more output.
    * Fixed GetTestBinCmd so that the working directory is the same as where the
      directory where the test were run.

--- test/test.go
+++ test/test.go
@@ -5,11 +5,15 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"path"
 	"path/filepath"
-	"runtime"
 	"strings"
 	"testing"
+)
+
+const (
+	FixturesDir = "testdata"
+	SubCmdFlags = "SUB_CMD_FLAGS"
+	TmpDir      = "tmp"
 )
 
 // AbsPath  Return the absolute path of the directory or panic if error.
@@ -23,18 +27,29 @@ func AbsPath(dir string) string {
 
 // GetTestBinCmd return a command to run the test binary in a sub-process, passing it flags as fixtures to produce expected output; `TestMain`, will be run automatically.
 func GetTestBinCmd(subEnvVarName string, args []string) *exec.Cmd {
+	vFlag := ""
+	if testing.Verbose() {
+		vFlag = "-test.v=true"
+	}
+
 	// call the generated test binary directly
 	// Have it the function runAppMain.
-	cmd := exec.Command(os.Args[0])
+	cmd := exec.Command(os.Args[0], vFlag)
+
+	wd, err1 := os.Getwd()
+	if err1 != nil {
+		panic("could not get current working directory: " + err1.Error())
+	}
+
 	// Run in the context of the source directory.
-	_, filename, _, _ := runtime.Caller(0)
-	cmd.Dir = path.Dir(filename)
+	cmd.Dir = wd
+
 	// Set an environment variable
 	// 1. Only exist for the life of the test that calls this function.
 	// 2. Passes arguments/flag to your app
-	// 3. Lets TestMain know when to run the main function.
-	subCmdFlags := subEnvVarName + "=" + strings.Join(args, " ")
-	cmd.Env = append(os.Environ(), subCmdFlags)
+	// 3. Lets RunAppMain, called in your TestMain function, know when to run the main function.
+	subCmdArgs := subEnvVarName + "=" + strings.Join(args, " ")
+	cmd.Env = append(os.Environ(), subCmdArgs)
 
 	return cmd
 }
@@ -72,6 +87,8 @@ func Silencer() func() {
 	}
 }
 
+// VerboseSubCmdOut Serves as a pass-through function display output to stdout
+// and stderr respectively, but only if the verbosity flag is turned on.
 func VerboseSubCmdOut(stdo []byte, stde error) ([]byte, error) {
 	// Debug
 	if testing.Verbose() {
@@ -86,4 +103,35 @@ func VerboseSubCmdOut(stdo []byte, stde error) ([]byte, error) {
 	}
 
 	return stdo, stde
+}
+
+// RunMain Used for running the application's main function from a call to
+// a test function, but in a sub process.
+//
+//	Function "m" must call os.Exit.
+func RunMain(subEnvVarName string, m func()) {
+	subCmdArgs, ok := os.LookupEnv(subEnvVarName)
+	if !ok { // Do nothing.
+		return
+	}
+
+	// This was adapted from https://golang.org/src/flag/flag_test.go; lines
+	// 596-657 at the time. This is called recursively, because we will have
+	// this test call itself in a sub-command when an environment variable name
+	// by `subEnvVarName` is set. Note that a call to `main()` MUST exit or
+	// you'll spin out of control.
+	args := strings.Split(subCmdArgs, " ")
+	ogArgs := os.Args
+	os.Args = append([]string{os.Args[0]}, args...)
+
+	defer func() {
+		os.Args = ogArgs
+	}()
+
+	js := strings.Join(ogArgs, " ")
+	if strings.Contains(js, "-test.v=true") {
+		fmt.Printf("\nsub process os.Args = %v\n", os.Args)
+	}
+
+	m()
 }
