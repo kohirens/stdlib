a90951ce2eb7cbe14b61f07434ec66b3e217b363
Khalifah K. Shabazz <shabazzk@gmail.com>
2024-02-05 22:49:49 -0500

    add: Detect Expired session ID Cookie
    Verify that the cookie that holds the session ID has not expired.

--- web/session/message.go
+++ web/session/message.go
@@ -2,6 +2,8 @@ package session
 
 var stderr = struct {
 	EmptySessionID string
+	ExpiredCookie  string
 }{
 	EmptySessionID: "session ID is empty",
+	ExpiredCookie:  "session ID cookie has expired on %v",
 }
