3d143942e96582a8d860732972d3c2f4bb098852
Khalifah Shabazz <shabazzk@gmail.com>
2025-02-20 16:02:51 -0500

    chg: Consolidate Session Restore
    Removed restored from cookie method. The Restore method now checks
    for nil storage, or if the session is expired. If either is so,
    then the call is a no-op.
    
    BREAKING: Removed methods from session package.

--- web/session/message.go
+++ web/session/message.go
@@ -4,10 +4,12 @@ var stderr = struct {
 	DecodeJSON     string
 	EmptySessionID string
 	ExpiredCookie  string
+	NoStorage      string
 	NoSuchKey      string
 }{
 	DecodeJSON:     "could not decode json data: %v",
 	EmptySessionID: "session ID is empty",
 	ExpiredCookie:  "session has expired at %v",
+	NoStorage:      "storage has not been set",
 	NoSuchKey:      "the key %v was not found in the session",
 }
