0315fd76f81507db22f13771b4a599549a5add09
Khalifah Shabazz <shabazzk@gmail.com>
2025-02-24 21:34:59 -0500

    fix: sss Package Session
    Return the correct *session.Data type in the sss Load session function.

--- sss/messages.go
+++ sss/messages.go
@@ -13,14 +13,16 @@ var Stdout = struct {
 }
 
 var Stderr = struct {
+	DecodeJSON         string
 	DownLoadKey        string
 	EncodeJSON         string
 	ReadObject         string
 	InvalidObjectState string
 	NoSuchKey          string
 }{
+	DecodeJSON:         "could not decode JSON: %v",
 	DownLoadKey:        "cannot download key %v from bucket %v: %v",
-	EncodeJSON:         "could not encode JSON: %w",
+	EncodeJSON:         "could not encode JSON: %v",
 	ReadObject:         "cannot read object key %v: %v",
 	InvalidObjectState: "s3 invalid object state: %v",
 	NoSuchKey:          "no such key %v",
