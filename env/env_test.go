c4832c26d0dcce32fa318ede3e2142a7cc984aca
Khalifah Shabazz <shabazzk@gmail.com>
2025-03-22 12:46:56 -0400

    add: Utilties to the env Package
    A few methods were added to the env package; origionally written
    for the Terraform sh (shell) provider. The methods read the environment
    variables from the shell. One reads all the variables and returns them
    as a map, and the other reads a specific variable and returns its value.

--- env/env_test.go
+++ env/env_test.go
@@ -1,7 +1,9 @@
 package env
 
 import (
+	"bytes"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -26,7 +28,36 @@ func TestGet(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			if got := Get(c.key, c.def); got != c.want {
 				t.Errorf("Get() = %v, want %v", got, c.want)
+				return
 			}
 		})
 	}
+}
+
+func TestParseEnvRE(t *testing.T) {
+	cases := []struct {
+		name    string
+		data    []byte
+		want    map[string]string
+		wantErr bool
+	}{
+		{"singleline", []byte("TEST=1234\nTEST2=4321"), map[string]string{"TEST": "1234", "TEST2": "4321"}, false},
+		{"multilineVar", []byte("TEST=1234\nTEST2=4321\n1234\n"), map[string]string{"TEST": "1234", "TEST2": "4321\n1234"}, false},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got, e1 := parseEnv(bytes.NewReader(c.data))
+
+			if e1 != nil && !c.wantErr {
+				t.Errorf("parseEnv() got err = %v, wanted = %v", e1, c.wantErr)
+			}
+
+			if reflect.DeepEqual(got, c.want) == false {
+				t.Errorf("got %v; want %v", got, c.want)
+				return
+			}
+		})
+	}
+
 }
